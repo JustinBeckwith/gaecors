3461eecee01db02bcc6b3b755d9c1d883869c08d
Justin Beckwith <beckwith@google.com>
2017-02-25 20:53:25 -0800

    add post example

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const formidable = require('formidable');
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get('/data', (req, res) => {
   res.json({msg: 'This is CORS-enabled for all origins!'});
+});
+
+//app.options('/products/:id', cors());
+
+app.post('/products/:id', (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields) => {
+    console.log(fields);
+    console.log('posting data to ' + req.params.id + ' with name ' + fields.name);
+    res.sendStatus(202);
+  });
 });
 
 app.get('/_ah/health', (req, res) => {
