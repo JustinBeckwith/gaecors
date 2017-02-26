3611a912c8682b2eca639ebc4834fbb1ad286b14
Justin Beckwith <beckwith@google.com>
2017-02-25 19:33:46 -0800

    serve tryit to attempt using from heroku

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors())
 
 app.get('/data', (req, res) => {
   res.json({msg: 'This is CORS-enabled for all origins!'});
+});
+
+app.get('/tryit', (req, res) => {
+  res.sendFile(path.join(__dirname, 'tryit.html'));
 });
 
 const port = process.env.PORT || 3000;
